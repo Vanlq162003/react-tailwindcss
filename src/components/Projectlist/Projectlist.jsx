b92d704a36b75c1f78e448787de683bc8342cf5e
Vanlq162003 <113500569+Vanlq162003@users.noreply.github.com>
2023-06-11 10:32:14 +0700

    fix vercel

--- src/components/Projectlist/Projectlist.jsx
+++ src/components/Projectlist/Projectlist.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import '../../pages/style.css';
 import ProjectCard from "./ProjectCard";
 import { Container, Row, Col } from 'react-bootstrap'
-import useradminpanel from '../../Assets/Admin-Panel-Dashboard.png'
-import olximage from '../../Assets/olx-clone image.png'
-import netfliximage from '../../Assets/React-App-netflix-clone.png'
-import gridlinesbuilders from '../../Assets/Gridlinesbuilders.png'
-import charlespizza from '../../Assets/Charlestown-Pizza.png'
-import todolist from '../../Assets/todolist.png'
+import useradminpanel from '../../assets/Admin-Panel-Dashboard.png'
+import olximage from '../../assets/olx-clone image.png'
+import netfliximage from '../../assets/React-App-netflix-clone.png'
+import gridlinesbuilders from '../../assets/Gridlinesbuilders.png'
+import charlespizza from '../../assets/Charlestown-Pizza.png'
+import todolist from '../../assets/todolist.png'
 
 function Projectlist() {
   return (
